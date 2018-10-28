Project-Xtended/packages_apps_Xtensions

incall vibration options [3/3] *allow setting vibration when call is connected *allow setting vibration when call is disconnected *allow setting vibration for call waiting *this works with google and aosp dialer :) SuperDroidBond-: Make it default on

src/com/msm/xtended/fragments/NotificationSettings.java
@@ -1,6 +1,9 @@
 package com.msm.xtended.fragments;
 
 import com.android.internal.logging.nano.MetricsProto;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceScreen;
+import com.msm.xtended.preferences.XUtils;
 
 import android.os.Bundle;
 import com.android.settings.R;
@@ -9,11 +12,19 @@ import com.android.settings.SettingsPreferenceFragment;
 
 public class NotificationSettings extends SettingsPreferenceFragment {
 
+    private static final String INCALL_VIB_OPTIONS = "incall_vib_options";
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
         addPreferencesFromResource(R.xml.x_settings_notifications);
+
+        PreferenceScreen prefScreen = getPreferenceScreen();
+        PreferenceCategory incallVibCategory = (PreferenceCategory) findPreference(INCALL_VIB_OPTIONS);
+        if (!XUtils.isVoiceCapable(getActivity())) {
+            prefScreen.removePreference(incallVibCategory);
+        }
     }
 
     @Override

VenomRom/android_packages_apps_VenomSettings

src/com/venom/settings/fragments/NotificationSettings.java
@@ -31,6 +31,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.venom.settings.preferences.Utils;
 
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.nano.MetricsProto;
@@ -39,13 +40,18 @@ import com.venom.settings.R;
 
 public class NotificationSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
+    private static final String INCALL_VIB_OPTIONS = "incall_vib_options";
 
      @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.venom_settings_notifications);
+        PreferenceScreen prefScreen = getPreferenceScreen();
+        PreferenceCategory incallVibCategory = (PreferenceCategory) findPreference(INCALL_VIB_OPTIONS);
+        if (!XUtils.isVoiceCapable(getActivity())) {
+            prefScreen.removePreference(incallVibCategory);
+        }
         ContentResolver resolver = getActivity().getContentResolver();
-        final PreferenceScreen prefScreen = getPreferenceScreen();
     }
      @Override
     public int getMetricsCategory() {